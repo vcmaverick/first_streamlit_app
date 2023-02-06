cae63eab586ada3ffa50263be666276c8c883534
vcmaverick <124264735+vcmaverick@users.noreply.github.com>
2023-02-06 10:47:26 +0530

    Update streamlit_app.py
    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,7 @@ streamlit.text('🥗Scrambled eggs')
 streamlit.text('🍇smoothies')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
