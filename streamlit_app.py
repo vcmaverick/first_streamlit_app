9674d62c67328dd866dc54d9f9df95beccfdc405
vcmaverick <124264735+vcmaverick@users.noreply.github.com>
2023-02-06 10:38:59 +0530

    Update streamlit_app.py
    added menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
-streamlit.title('My Parents New diner')
+streamlit.title('New diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Bluberry oatmeal')
+streamlit.text('Avacado toast')
+streamlit.text('Scrambled eggs')
+streamlit.text('smoothies')
+
